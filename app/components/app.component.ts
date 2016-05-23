8d7a5ca27bef140eacf85d11f2793b35d3c11fb3
azahreba <azahreba@google.com>
2016-05-23 17:07:28 +0200

    (add): angular material

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { MdButton } from '@angular2-material/button';
 import { MdCheckbox } from '@angular2-material/checkbox';
+import { MdProgressCircle, MdSpinner } from '@angular2-material/progress-circle';
 
 @Component({
     selector: 'rm-app',
     templateUrl: 'app/components/app.component.html',
-    directives: [MdButton, MdCheckbox]
+    directives: [MdButton, MdCheckbox, MdProgressCircle, MdSpinner]
 })
 export class AppComponent {
     todos = [
--- systemjs.config.js
+++ systemjs.config.js
@@ -15,7 +15,8 @@
     // The core package is required as a peerDependency for other components.
     '@angular2-material/core': 'node_modules/@angular2-material/core/core',
     '@angular2-material/checkbox': 'node_modules/@angular2-material/checkbox/checkbox',
-    '@angular2-material/button': 'node_modules/@angular2-material/button/button'
+    '@angular2-material/button': 'node_modules/@angular2-material/button/button',
+    '@angular2-material/progress-circle': 'node_modules/@angular2-material/progress-circle/progress-circle'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
