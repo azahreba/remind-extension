41d4df63a52c76556bdc3f822ebbafe5d86bc0c1
azahreba <azahreba@google.com>
2016-05-24 17:58:18 +0200

    (add): angular material components

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 
-import { MdButton } from '@angular2-material/button';
-import { MdCheckbox } from '@angular2-material/checkbox';
-import { MdProgressCircle, MdSpinner } from '@angular2-material/progress-circle';
+import {MdButton} from '@angular2-material/button';
+import {MdCard} from '@angular2-material/card';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MdInput} from '@angular2-material/input';
+import {MdProgressCircle, MdSpinner} from '@angular2-material/progress-circle';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MdToolbar} from '@angular2-material/toolbar';
 
 @Component({
     selector: 'rm-app',
     templateUrl: 'app/components/app.component.html',
-    directives: [MdButton, MdCheckbox, MdProgressCircle, MdSpinner]
+    directives: [
+        MdButton,
+        MdCard,
+        MdCheckbox,
+        MdIcon,
+        MdInput,
+        MdProgressCircle,
+        MD_SIDENAV_DIRECTIVES,
+        MD_LIST_DIRECTIVES,
+        MdSpinner,
+        MdToolbar
+    ],
+    styleUrls: ['app/components/app.component.css'],
+    providers: [HTTP_PROVIDERS, MdIconRegistry]
 })
+
 export class AppComponent {
     todos = [
         {
             completed: false,
             name: 'One'
-        },{
+        }, {
             completed: false,
             name: 'Second'
-        },{
+        }, {
             completed: false,
             name: 'Third'
         },
--- systemjs.config.js
+++ systemjs.config.js
@@ -13,10 +13,16 @@
     '@angular':                   'node_modules/@angular',
 
     // The core package is required as a peerDependency for other components.
-    '@angular2-material/core': 'node_modules/@angular2-material/core/core',
-    '@angular2-material/checkbox': 'node_modules/@angular2-material/checkbox/checkbox',
     '@angular2-material/button': 'node_modules/@angular2-material/button/button',
-    '@angular2-material/progress-circle': 'node_modules/@angular2-material/progress-circle/progress-circle'
+    '@angular2-material/card': 'node_modules/@angular2-material/card/card',
+    '@angular2-material/checkbox': 'node_modules/@angular2-material/checkbox/checkbox',
+    '@angular2-material/core': 'node_modules/@angular2-material/core',
+    '@angular2-material/icon': 'node_modules/@angular2-material/icon/icon',
+    '@angular2-material/input': 'node_modules/@angular2-material/input/input',
+    '@angular2-material/list': 'node_modules/@angular2-material/list/list',
+    '@angular2-material/progress-circle': 'node_modules/@angular2-material/progress-circle/progress-circle',
+    '@angular2-material/sidenav': 'node_modules/@angular2-material/sidenav/sidenav',
+    '@angular2-material/toolbar': 'node_modules/@angular2-material/toolbar/toolbar'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
